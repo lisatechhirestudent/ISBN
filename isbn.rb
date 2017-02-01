1b72c5a01d259e12d188f07e2eb26f73b28b2005
techhirestudent <live_work_achieve@outlook.com>
2017-02-01 17:21:07 -0500

    good_check_sum

--- isbn.rb
+++ isbn.rb
@@ -7,7 +7,27 @@ def remove_spaces_and_dashes(num)
 
 		end
 
-
 	end
 	formatted_number
+end
+def calculate_checksum(num)
+	sum = 0
+	index = 1
+
+	if num[num.length - 1].upcase =="X"
+		num.delete!(num[num.length - 1])
+	end
+
+	num.each_char do |number|
+			sum += (number.to_i * index)
+			index += 1
+		end
+		sum % 11			
+end
+def correct_checksum?(num)
+	if num[num.length - 1].upcase == "X"
+		calculate_checksum(num) == 10
+	else
+		calculatechecksum(num) == num[length - 1]
+	end	
 end
--- testisbn.rb
+++ testisbn.rb
@@ -6,8 +6,13 @@ class TestISBN < Minitest::Test
 	def test_returns_spaces_and_dashes
 	assert_equal("0471958697", remove_spaces_and_dashes("04 7-1-9 58 69-7"))
 
-
-
 	end
 
-end
+	def test_correct_checksum
+	assert_equal(7, calculate_checksum("0471958690"))
+	end
+	def test_is_good_checksum
+		assert_equal(true, correct_checksum?("877195869x"))
+	end
+
+ end
