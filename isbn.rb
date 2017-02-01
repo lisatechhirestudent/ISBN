9f60ccf39aea00da8bf6d02af56bcf6518a55ad7
techhirestudent <live_work_achieve@outlook.com>
2017-02-01 15:53:28 -0500

    test_remove

---
+++ isbn.rb
@@ -0,0 +1,13 @@
+def remove_spaces_and_dashes(num)
+
+	formatted_number =""
+	num.each_char do |letter|
+		if letter != " " && letter != "-"
+			formatted_number += letter
+
+		end
+
+
+	end
+	formatted_number
+end
